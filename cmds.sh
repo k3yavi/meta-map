7759e768b0844e09d5e76b2955357dd4dc81d1b3
k3yavi <avi.srivastava@stonybrook.edu>
2017-12-29 14:56:31 -0500

    2paco ready

--- cmds.sh
+++ cmds.sh
@@ -1,2 +1,9 @@
 # command to download the refs (no Eukaryotes yet)
 parallel -j 10 "wget https://www.ebi.ac.uk/ena/data/view/{}\&display\=fasta -O {}.fa" ::: `cat ./refs/ref_list.txt`
+
+#run TwoPaCo
+mkdir -p gfa
+mkdir -p temp
+./bin/TwoPaCo/build/graphconstructor/twopaco -k 25 -f 20 -t 15 --tmpdir temp/ --outfile gfa refs/fasta/*
+
+./bin/TwoPaCo/build/graphdump/graphdump -k 25 <(cat ./bin/TwoPaCo/2paco-s.format) -f gfa1 ./gfa/de_bruijn.bin > gfa/k25.gfa
